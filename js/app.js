a11ba867eda596354ec9594444e19d6c1d10462c
Matt Zager <mzager09@gmail.com>
2016-03-06 13:50:07 -0500

    new commit

--- js/app.js
+++ js/app.js
@@ -27,8 +27,8 @@ $('.ryu-ready').show();
 
 });
 
-function playHadouken () {
+function playHadouken() {
 	$('#hadouken-sound')[0].volume = 0.5;
 	$('#hadouken-sound')[0].load();
-	$('hadouken-sound')[0].play();
+	$('#hadouken-sound')[0].play();
 }
